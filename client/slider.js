ada3644e9167af14e44f13df9243207e0ffbbb5f
gleb-sokolkov <zmihpog@gmail.com>
2021-09-15 19:05:56 +0600

    slider was done

--- client/slider.js
+++ client/slider.js
@@ -1,2 +1,7 @@
-import './scss/styles.scss';
 import './scss/slider/slider.scss';
+import Slider from './js/slider';
+
+window.onload = () => {
+  // eslint-disable-next-line no-unused-vars
+  const sliders = Slider.findAllSliders();
+};
