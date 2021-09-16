3aad7d6845a84012cb61919bda56a3eb623840a6
gleb-sokolkov <zmihpog@gmail.com>
2021-09-17 00:28:57 +0600

    added base landing layout

--- client/index.js
+++ client/index.js
@@ -1,2 +1,10 @@
 import './polyfills';
 import './scss/styles.scss';
+import './scss/landing/styles.scss';
+
+function importAll(r) {
+  return r.keys().map(r);
+}
+
+// eslint-disable-next-line no-unused-vars
+const images = importAll(require.context('./images', false, /\.(png|jpe?g|svg)$/));
